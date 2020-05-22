a2a2588adbccd66e9818917fc6cf09dcde593202
DAZUN XING <dazun.xing@gmail.com>
2020-05-22 07:12:25 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,13 @@
 class Artist 
   attr_accessor :name 
-  @@song_count = 0
+  # @@song_count = 0
   def initialize(name)
     @name =name
   end
   
   def add_song(song)
     song.artist =self
-    @@song_count +=1
+    # @@song_count +=1
   end
   
   def songs 
@@ -17,10 +17,11 @@ class Artist
   def add_song_by_name(name)
     song =Song.new(name)
     add_song(song)
-    @@song_count +=1
+    # @@song_count +=1
   end
   
   def self.song_count 
-    @@song_count
+    # @@song_count
+       Song.all.count {|song| !song.artist.nil?}
   end
 end
